89cd7804554751ccd021c8b65ceac873ec841253
Vikas <vikas@spartanbots.com>
2023-05-28 00:45:34 +0530

    fixed authentication problem, updateAdminPass API

--- app/Models/SuperAdmin.php
+++ app/Models/SuperAdmin.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Laravel\Sanctum\HasApiTokens;
 
-class SuperAdmin extends Model
+class SuperAdmin extends Authenticatable implements AuthenticatableContract
 {
     use HasApiTokens, HasFactory;
     public $table = 'super_admins';
